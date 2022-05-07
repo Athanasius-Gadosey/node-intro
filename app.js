849f27071bfc5d9e0e2f968fd445b6dbb405fbfd
A.J.K.G <athanasuisgadosey@gmail.com>
2022-05-07 00:31:13 +0000

    node-intro created

---
+++ app.js
@@ -0,0 +1,18 @@
+// setTimeout(function(){
+// 	console.log('hello node')
+// }, 5000);
+
+
+// **function Expressions**
+// let calculator = function(square){
+
+// 	return square*square;
+// }
+
+// let doubleNum = function(n){
+// 	return n*2
+// }
+// console.log(doubleNum(calculator(5)));
+
+// **Modules**
+
